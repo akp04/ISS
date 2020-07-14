11a55cd44a1fa470fc788fd541b0c522104d8f48
AAKASH PANDITA <aakash2711522@gmail.com>
2020-07-14 22:43:05 +0530

    uploading using async await
    upload 2

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-let a = () =>{
+async function a(){
 	
-	fetch("http://api.open-notify.org/iss-now.json/")
-	.then(response => response.json())
-	.then(data =>{
+	const response = await fetch("http://api.open-notify.org/iss-now.json/");
+	const data = await response.json();
 
 			document.getElementById("root1").innerHTML = 
 			"Latitude : " + JSON.stringify(data.iss_position.latitude)
 			+ '<br/>' + "Longitude : " +  JSON.stringify(data.iss_position.longitude)
 			+ '<br/>' + "Time stamp : " +  JSON.stringify(data.timestamp);
 		
-		}
-		);
 
-	fetch("http://api.open-notify.org/astros.json/")
-	.then(response => response.json())
-	.then(data =>{
+	const response1 = await fetch("http://api.open-notify.org/astros.json/");
+	const data1 = await response1.json();
 		
 			document.getElementById("root2").innerHTML = 
-			"Number of people in space right now : " + JSON.stringify(data.number);
+			"Number of people in space right now : " + JSON.stringify(data1.number);
 
 			let str="";
-			for(let i = 0;i<data.number;i++)
+			for(let i = 0;i<data1.number;i++)
 			{
-				str = str + (i+1) + ". " + JSON.stringify(data.people[i].name) + '<br/>';	
+				str = str + (i+1) + ". " + JSON.stringify(data1.people[i].name) + '<br/>';	
 			}
 
 			document.getElementById("root3").innerHTML = str;
 		
-		}
-		);
 }
